2f4c1a2627271c7b3e853287204abdec57643225
Benny Steve <bennysteve2005@gmail.com>
2024-05-16 22:44:31 +0530

    Update main.js

--- To-do/main.js
+++ To-do/main.js
@@ -48,11 +48,20 @@ function clearModal(){
 }
 
 function createCard(len){
+    //creating Elements
     let d=document.createElement("div")
     let h2=document.createElement("h4")
     let h=document.createElement("h3")
     let p1=document.createElement("p")
     let p2=document.createElement("p")
+    let d2=document.createElement("div")
+    let p3=document.createElement("p")
+    let ch=document.createElement("input")
+    let select=document.createElement("select")
+    let opt1=document.createElement("option")
+    let opt2=document.createElement("option")
+    let optMain=document.createElement("option")
+    ch.setAttribute("type", "checkbox")
     d.classList.add("card")
     h2.textContent="Task "+(len+1)
     h.textContent=taskHolder+" "+list[len].name
@@ -62,9 +71,20 @@ function createCard(len){
     d.appendChild(h)
     d.appendChild(p1)
     d.appendChild(p2)
+
+    p3.textContent="Completed"
+    d2.appendChild(p3)
+    d2.appendChild(ch)
+    d.appendChild(d2)
     container.appendChild(d)
+    optMain.text="----Select-Priority----"
+    select.add(optMain)
+    opt1.text='high-priority'
+    select.add(opt1)
+    opt2.text='low-priority'
+    select.add(opt2)
+    d.appendChild(select)
 }
 
 list.push(new task("Practice","2024-12-12","12:12"))
-console.log(list)
  createCard(0)
