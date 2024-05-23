421a6d6efa4f3a937dc96e122096423f1f6af2be
Benny Steve <bennysteve2005@gmail.com>
2024-05-23 16:29:58 +0530

    Update main.js

--- To-do/main.js
+++ To-do/main.js
@@ -1,4 +1,4 @@
-let list=[
+var list=[
     
 ]
 let container=document.querySelector(".container")
@@ -11,8 +11,12 @@ let close=document.querySelector('.close')
 let addBtn=document.querySelector(".insert")
 let taskHolder="Task:"
 let timeHolder="Time:"
+let nowDate=new Date()
 let dateHolder="Date:"
-
+let today=nowDate.toISOString().split('T')[0]
+let now_time=nowDate.getHours()+":"+nowDate.getMinutes()
+// console.log(now_time)
+// console.log(nowDate.toISOString().split('T')[0])
 
 
 btn.addEventListener("click",openModel)
@@ -25,10 +29,14 @@ close.addEventListener("click",()=>{
 
 addBtn.addEventListener("click",()=>{
     let length=0
-    list.push(new task(toDo.value,date.value,time.value))
-    // console.log(list)
-    length=list.length
-    createCard(length-1)
+    checkDate(date.value)
+    check_toDo(toDo.value)
+    if(checkDate(date.value,toDo.value)&&check_toDo(toDo.value)){
+        list.push(new task(toDo.value,date.value,time.value))
+        // console.log(list)
+        length=list.length
+        createCard(length-1)
+    }
     clearModal()
     console.log(list)
 })
@@ -50,18 +58,33 @@ function clearModal(){
 function createCard(len){
     //creating Elements
     let d=document.createElement("div")
+    // let d3=document.createElement("div")
     let h2=document.createElement("h4")
     let h=document.createElement("h3")
     let p1=document.createElement("p")
     let p2=document.createElement("p")
     let d2=document.createElement("div")
     let p3=document.createElement("p")
-    let ch=document.createElement("input")
-    let select=document.createElement("select")
+    var ch=document.createElement("input")
+    var select=document.createElement("select")
+    select.addEventListener('change',()=>{
+        if(select.value=='high-priority'){
+            numberOfPriorityTask--
+            select.parentElement.style.order=numberOfPriorityTask
+            
+        }
+        else{
+            select.parentElement.style.order=0
+        }
+        
+    })
     let opt1=document.createElement("option")
     let opt2=document.createElement("option")
     let optMain=document.createElement("option")
+
+    select.classList.add("priority")
     ch.setAttribute("type", "checkbox")
+    ch.classList.add('finished')
     d.classList.add("card")
     h2.textContent="Task "+(len+1)
     h.textContent=taskHolder+" "+list[len].name
@@ -84,7 +107,77 @@ function createCard(len){
     opt2.text='low-priority'
     select.add(opt2)
     d.appendChild(select)
-}
 
+
+    ch.addEventListener('click',()=>{
+        if(ch.checked==true){
+            ch.parentElement.parentElement.style.opacity=.6
+            ch.style.opacity=1
+        }
+        else{
+            ch.parentElement.parentElement.style.opacity=1
+        }
+})
+
+}
 list.push(new task("Practice","2024-12-12","12:12"))
- createCard(0)
+list.push(new task("Practice","2024-12-12","12:12"))
+list.push(new task("Practice","2024-12-12","12:12"))
+createCard(0)
+createCard(1)
+createCard(2)
+
+ //checking date
+ function checkDate(dateToCheck){
+    if(dateToCheck<today){
+        alert("Enter valid date and time")
+        return false
+    }else{
+        return true
+    }
+ }
+
+ function check_toDo(taskTOCheck){
+    console.log("Its a fun")
+    if(taskTOCheck!=""){
+        console.log("task there")
+        return true
+    }
+    else{
+        console.log("task not there")
+        return false
+    }
+ }
+
+//  checkDate("2024-12-12")
+let numberOfPriorityTask=0
+// let checkPriority=document.querySelectorAll("select")
+// checkPriority.forEach(element =>element.addEventListener('change',()=>{
+//     if(element.value=='high-priority'){
+//         numberOfPriorityTask--
+//         element.parentElement.style.order=numberOfPriorityTask
+//         console.log(element)
+//     }
+//     else{
+//         console.log("low")
+//     }
+//     console.log("dasdd")
+// }))
+
+// function high_Priority(){
+//     if(element.value=='high-priority'){
+//         numberOfPriorityTask--
+//         element.parentElement.style.order=numberOfPriorityTask
+//         console.log(element)
+//     }
+//     else{
+//         console.log("low")
+//     }
+//     console.log("dasdd")
+// }
+
+// var checkbox=document.querySelectorAll(".finished")
+// checkbox.forEach(element=>element.addEventListener('click',()=>{
+//     console.log("Checkbox clicked")
+// }
+// ))
